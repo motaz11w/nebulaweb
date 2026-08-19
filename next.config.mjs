5122e7abdc1572441b36e5e99b6c98a80d22d83b
motaz11w <motaz166619697@gmail.com>
2026-08-19 14:25:36 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/nebulaweb',
+  assetPrefix: '/nebulaweb/',
   images: {
     unoptimized: true,
   },
 };
+
 export default nextConfig;
